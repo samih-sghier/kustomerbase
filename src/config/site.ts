5a5f1d1bcca5c7e5266d094ac43bdc8e769c3455
Samih Sghier <samihsghier2020@gmail.com>
2024-11-07 00:54:47 -0500

    changed keywords

--- src/config/site.ts
+++ src/config/site.ts
@@ -9,7 +9,7 @@ export const siteConfig = {
     description:
         "Custom ChatGPT for your business",
     orgImage:
-        "https://utfs.io/f/30ef05da-6556-4b2f-9428-bf5b706f3bd4-xt0cyj.png",
+        "https://utfs.io/f/z1SQx2HK8PtsY15Q8uvaYyIlbV8gfnFD97GK6rEtwcUpWJx5",
     contactEmail: "support@inboxpilot.co",
     noReplyEmail: "no-reply@inboxpilot.co",
     catchyPhrases: [
